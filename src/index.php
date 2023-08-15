5fb80508d472cee8b2a97878585fd044fa72759d
Artur Carvalho <artur.carvalho0@outlook.com>
2023-08-15 13:35:45 -0300

    Atualização

--- src/index.php
+++ src/index.php
@@ -2,6 +2,11 @@
 require('./controllers/clientController.php');
 require('./controllers/agendaController.php');
 
+if(!isset($_GET['c'])) {
+  $c = '';
+}else {
+  $c = $_GET['c'];
+}
 $action = !empty($_GET['a']) ? $_GET['a'] : 'getAll';
 if($c == 'a'){
   $controller = new agendaController();
